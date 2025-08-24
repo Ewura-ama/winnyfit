c30c6c1afae9cefa17d78cb2c5553f6cce721a88
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2025-08-24 18:24:31 +0000

    updaet

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const SignIn = lazy(() => import('./pages/accounts/SignIn'))
 const SignUp = lazy(() => import('./pages/accounts/SignUp'))
 const Profile = lazy(() => import('./pages/accounts/Profile'))
 const Booking = lazy(() => import('./pages/booking/BookingPage'))
-const ProfileBooking = lazy(() => import('./pages/profile/ProfileBookingPage'))
+
 const Classes = lazy(() => import('./pages/classes/ClassesPage'))
 const Trainers = lazy(() => import('./pages/trainers/TrainersPage'))
 const Products = lazy(() => import('./pages/products/ProductsPage'))
@@ -22,7 +22,6 @@ const routes = [
     { path: '/signin', component: SignIn},
     { path: '/signup', component: SignUp},
     { path: '/profile', component: Profile},
-    { path: '/profile-booking', component: ProfileBooking},
     { path: 'booking/', component: Booking},
     { path: 'classes/', component: Classes},
     { path: 'trainers/', component: Trainers},
